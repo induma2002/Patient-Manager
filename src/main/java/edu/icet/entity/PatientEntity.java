6ccf9075bf7bf8ea237e1aba6411d0d20eeaef76
induma2002 <indumawijesinghe@outlook.com>
2024-10-21 22:42:21 +0530

    Creat Database Connection

---
+++ src/main/java/edu/icet/entity/PatientEntity.java
@@ -0,0 +1,25 @@
+package edu.icet.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class PatientEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String nic;
+    private String address;
+    private String bloodGroup;
+    private String category;
+    private String gender;
+    private String contact;
+    private String note;
+    private String age;
+    private String allergies;
+}
--- src/main/java/edu/icet/service/impl/patientServiceImpl.java
+++ src/main/java/edu/icet/service/impl/patientServiceImpl.java
@@ -1,24 +1,36 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.Patient;
+import edu.icet.entity.PatientEntity;
+import edu.icet.repository.PatientRepository;
 import edu.icet.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class patientServiceImpl implements PatientService {
 
-    List<Patient> patientList = new ArrayList<>();
+    final PatientRepository patientRepository;
+    final ModelMapper mapper;
+
+
+    List<Patient> patientList;
 
     @Override
     public List <Patient> getPatient() {
+        patientList = new ArrayList<>();
+        patientRepository.findAll().forEach(patientEntity -> patientList.add(mapper.map(patientEntity, Patient.class)));
         return patientList;
     }
 
     @Override
     public void addPatient(Patient patient) {
-        patientList.add(patient);
+        patientRepository.save(mapper.map(patient, PatientEntity.class));
+
     }
 }
