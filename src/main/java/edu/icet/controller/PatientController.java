0bfd0563041b5011e708fb0558cb44b2bc62ebef
induma2002 <indumawijesinghe@outlook.com>
2024-10-23 07:02:02 +0530

    Creat Update Patient API

--- src/main/java/edu/icet/controller/PatientController.java
+++ src/main/java/edu/icet/controller/PatientController.java
@@ -2,16 +2,17 @@ package edu.icet.controller;
 
 import edu.icet.dto.Patient;
 import edu.icet.service.PatientService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class PatientController {
 
-    @Autowired
-    PatientService patientService;
+    final PatientService patientService;
 
     @GetMapping("/get-all-patient")
     public List <Patient> getPatient() {
@@ -26,6 +27,10 @@ public class PatientController {
     @DeleteMapping("delete-by-id/{id}")
     public void deletePatient(@PathVariable Integer id){
         patientService.deleteById(id);
+    }
+    @PutMapping("/update-patient")
+    public void updatePatient(@RequestBody Patient patient) {
+        patientService.addPatient(patient);
     }
 
 }
